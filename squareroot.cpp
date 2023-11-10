e708d5b41d16e14c5ca35470a7dab915038e0370
aniketkumar <kaniket123456789@gmail.com>
2023-11-10 23:04:53 +0530

    Indentation change

--- squareroot.cpp
+++ squareroot.cpp
@@ -1,26 +1,31 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int num;
-    cout<<"Enter the number:"<<endl;
-    cin>>num;
-    int i=0;
-    int ans=0;
-    while(i<=num){
-        long sq=(long)i*i;
+    cout << "Enter the number:" << endl;
+    cin >> num;
+    int i = 0;
+    int ans = 0;
+    while (i <= num)
+    {
+        long sq = (long)i * i;
 
-        if(sq==num){
-            ans=i;
+        if (sq == num)
+        {
+            ans = i;
             break;
         }
-        else if(sq>num){
-            ans=i-1;
+        else if (sq > num)
+        {
+            ans = i - 1;
             break;
         }
-        else{
+        else
+        {
             i++;
         }
     }
-    cout<<"The square root of "<<num<<" is:"<<ans;
+    cout << "The square root of " << num << " is:" << ans;
 }
