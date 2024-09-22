81554a5eb4386c35ae77e70d48e9e0bc0c41d56e
aniketkumar <kaniket123456789@gmail.com>
2024-09-22 14:35:08 +0530

    just a small change

--- first.cpp
+++ first.cpp
@@ -1,7 +1,19 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    cout<<"hello world!";
+int main()
+{
+    // printing the last digit of a number
+    //  and the digits except the last digit
+    int num, rem;
+    cout << "Enter the number\n";
+    cin >> num;
+    rem = num % 10;
+    num /= 10;
+    cout << "The last digit is\n";
+    cout << rem;
+    cout << "\nThe digit except the last digit\n";
+    cout << num2;
+
     return 0;
 }
